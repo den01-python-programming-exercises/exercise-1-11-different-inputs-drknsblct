7b16e948744d150dc036af3bef86087c85a56cfb
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-16 05:50:50 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
 def main():
     #write your code below this line
+    
+    text = input('Give a string:')
+    num = int(input('Give an integer:'))
+    num2 = float(input('Give a float:'))
+    ans = bool(input('Give a boolean:'))
+    
+    print(f'You gave the string {text}')
+    print(f'You gave the integer {num}')
+    print(f'You gave the float {num2}')
+    print(f'You gave the boolean {ans}')
+    
+    
 
 if __name__ == '__main__':
     main()
+
+    
