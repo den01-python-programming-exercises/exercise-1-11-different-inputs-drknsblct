fec856785e663dcda00aa48ad66d64c1843b23bb
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-16 05:53:45 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -6,10 +6,15 @@ def main():
     num2 = float(input('Give a float:'))
     ans = bool(input('Give a boolean:'))
     
-    print(f'You gave the string {text}')
-    print(f'You gave the integer {num}')
-    print(f'You gave the float {num2}')
-    print(f'You gave the boolean {ans}')
+#     print(f'You gave the string {text}')
+#     print(f'You gave the integer {num}')
+#     print(f'You gave the float {num2}')
+#     print(f'You gave the boolean {ans}')
+
+    print('You gave the string ' + text')
+    print('You gave the integer ' + str(num)')
+    print('You gave the float ' + str(num2)')
+    print('You gave the boolean ' + str(ans)')
     
     
 
